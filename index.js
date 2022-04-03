a8d60efe8854661069a7ec8cb893f3a8296162c1
eskevv <hkeven89@gmail.com>
2022-04-03 18:53:08 -0400

    styling fixed, script cleaned

--- index.js
+++ index.js
@@ -1,24 +1,56 @@
 const grid = document.querySelector('.sketch');
-const cell_borderSize = 2;
-const max_width = (16 + cell_borderSize) * 16 + cell_borderSize;
-grid.style.maxWidth = `${max_width}px`;
+const clear = document.querySelector('.tools #clear')
+const size_label = document.querySelector('.slider label')
+const size_slider = document.querySelector('#sizeSlider')
 
-for (let i = 0; i < 256; i++) {
-   const square = document.createElement('div');
-   square.addEventListener('mouseover', changeColor);
-   if (i >= 240) {
-      square.style.borderBottom = `${cell_borderSize}px solid rgb(92, 108, 129)`;
+const cell_borderSize = 2;
+const border_style = `${cell_borderSize}px solid rgb(52, 55, 58)`;
+
+createGrid();
+
+size_slider.addEventListener('change', () => {
+   createGrid();
+})
+
+size_slider.addEventListener('input', () => {
+   let grid_size = size_slider.value;
+   size_label.textContent = grid_size + 'x' + grid_size;
+})
+
+clear.addEventListener('click', clearGrid);
+
+function createGrid() {
+   let grid_size = size_slider.value;
+   let max_width = (16 + cell_borderSize) * grid_size + cell_borderSize;
+   let num_tiles = grid_size * grid_size;
+
+   size_label.textContent = grid_size + 'x' + grid_size;
+   grid.style.width = `${max_width}px`;
+   while (grid.firstChild) {
+      grid.removeChild(grid.firstChild);
    }
-   if (i >= 0 && i < 16) {
-      square.style.borderTop = `${cell_borderSize}px solid rgb(92, 108, 129)`;
+   for (let i = 0; i < num_tiles; i++) {
+      const square = document.createElement('div');
+      square.addEventListener('mouseover', changeColor);
+      if (i >= num_tiles - grid_size) {
+         square.style.borderBottom = border_style;
+      }
+      if (i >= 0 && i < grid_size) {
+         square.style.borderTop = border_style;
+      }
+      if (i % grid_size === 0) {
+         square.style.borderLeft = border_style;
+      }
+      if (i % grid_size === grid_size - 1) {
+         square.style.borderRight = border_style;
+      }
+      grid.appendChild(square);
    }
-   if (i % 16 === 0) {
-      square.style.borderLeft = `${cell_borderSize}px solid rgb(92, 108, 129)`;
-   }
-   if (i % 16 === 15) {
-      square.style.borderRight = `${cell_borderSize}px solid rgb(92, 108, 129)`;
-   }
-   grid.appendChild(square);
+}
+
+function clearGrid() {
+   const cells = document.querySelectorAll('.sketch div');
+   cells.forEach(c => c.setAttribute('id', ''));
 }
 
 function changeColor() {
@@ -34,7 +66,7 @@ function changeColor() {
       case 'filledThree':
          this.setAttribute('id', 'filledThree');
          break;
-      default :
+      default:
          this.setAttribute('id', 'filledOne');
          break;
    }
